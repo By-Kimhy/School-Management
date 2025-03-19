7b90927505d111ed4762250ca0cefe39ec9b67a5
kimhy0325 <kimhyby99@gmail.com>
2025-03-19 20:23:47 +0700

    changer table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('level');
+            $table->enum('active');
+            $table->string('language');
             $table->timestamps();
         });
     }
